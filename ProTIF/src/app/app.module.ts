2a8dcbc80761a8afd64b6f48a6f867d34c2c3ffc
Roberto Mastrángelo Reyes <106489715+mastrangeloispc@users.noreply.github.com>
2023-02-02 23:52:23 -0300

    3 24

--- ProTIF/src/app/app.module.ts
+++ ProTIF/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
