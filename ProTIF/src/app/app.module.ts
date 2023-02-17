8992cf321e3d3be3546455e83957654d6aca5242
Roberto Mastrángelo Reyes <106489715+mastrangeloispc@users.noreply.github.com>
2023-02-17 04:23:13 -0300

    hard y soft revisar
    revisar hardysoft html

--- ProTIF/src/app/app.module.ts
+++ ProTIF/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardysoftComponent } from './components/hardysoft/hardysoft.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +22,17 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HardysoftComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+  
+    })
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ProTIF/src/app/components/hardysoft/hardysoft.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hardysoft',
+  templateUrl: './hardysoft.component.html',
+  styleUrls: ['./hardysoft.component.css']
+})
+export class HardysoftComponent {
+
+}
